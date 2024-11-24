b0f18e86ddaeb47b275155acb6cc71b470fe33af
Tamás Szűcs <65996932+tamas-tech@users.noreply.github.com>
2024-11-24 21:34:58 +0100

    Update samples0.js

--- samples/samples0.js
+++ samples/samples0.js
@@ -2,3 +2,8 @@ function kuldes(elem) {
     var message = elem.nextElementSibling.innerText
     window.parent.postMessage(message, '*');
 };
+
+function kuldesgo() {
+    var message = "gotook";
+    window.parent.postMessage(message, '*');
+};
