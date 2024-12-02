a0c4ee9ebf0370c9175fd2e29096c2d0347d8556
Tamás Szűcs <65996932+tamas-tech@users.noreply.github.com>
2024-12-02 21:32:14 +0100

    Update samples0.js

--- samples/samples0.js
+++ samples/samples0.js
@@ -1,9 +1,18 @@
 function kuldes(elem) {
-    var message = elem.nextElementSibling.nextElementSibling.innerText
+    var message = elem.nextElementSibling.nextElementSibling.nextElementSibling.innerText
     window.parent.postMessage(message, '*');
 };
 
 function kuldesgo() {
     var message = "gotook";
     window.parent.postMessage(message, '*');
+};
+
+function pretoggle(elem) {
+    elem.nextElementSibling.classList.toggle('show')
+    elem.classList.toggle('hide')
+    if (elem.innerText == "Show code")
+        elem.innerText = "Hide code";
+    else
+        elem.innerText = "Show code";
 };
