b13bc68946c21723cfa447ce052945da853c823a
Tamás Szűcs <65996932+tamas-tech@users.noreply.github.com>
2026-06-11 12:21:16 +0200

    sorszámozás \u25b6-tal

--- samples/halgebra.js
+++ samples/halgebra.js
@@ -9407,8 +9407,10 @@ function prelatexjs(c_txt, mathjax) {
             var L = v.split("_");
             getsetZycFabFib(L[0], parseInt(L[1]), false);
         };
-    if (mathjax)
+      if (mathjax) {
         c_txt = c_txt.replace(/^#.*[\n\r\f]+/mg, '');
+        c_txt = c_txt.replace(/^.*▶/mg, '');
+    }
     //c_txt = c_txt.replace(/(\#.*[\n\r\f])/g, '');
 
     var Vars = c_txt.match(/\§\§.*?\§\§[\n\r\f]*/g);
